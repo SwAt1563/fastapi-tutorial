54e0fd456206259d82c7980b52cbcdc12077ddb1
Qutaiba Olayyan <swat.ar123@gmail.com>
2024-05-23 19:29:04 +0300

    FastAPI Metadata and Docs URLs

--- main.py
+++ main.py
@@ -9,7 +9,56 @@ from uuid import UUID
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 
-app = FastAPI()
+
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+app = FastAPI(
+    # openapi_url=None,
+    openapi_tags=tags_metadata,
+    title="ChimichangApp",
+    description=description,
+    summary="Deadpool's favorite app. Nuff said.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        # "identifier": "MIT",
+    },
+)
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
@@ -48,7 +97,7 @@ async def read_root():
 
 # this will override the '/items/{item_id}' route
 # just of this specific route otherwire the second route will be used
-@app.get("/items/favorite")
+@app.get("/items/favorite", tags=["items"])
 async def read_item():
     return {"item": "override"}
 
