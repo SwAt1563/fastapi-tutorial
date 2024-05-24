c298bd7fff2b74498e4ba8a3e0b0fd6ed41aa272
Qutaiba Olayyan <swat.ar123@gmail.com>
2024-05-24 16:42:50 +0300

    async testing

--- main.py
+++ main.py
@@ -787,3 +787,23 @@ That's what we'll solve, let's load the model before the requests are handled, b
 #     return {"result": result}
 
 
+
+# async testing
+"""
+# https://fastapi.tiangolo.com/advanced/async-tests/
+
+import pytest
+from httpx import AsyncClient
+
+from .main import app
+
+
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Tomato"}
+
+
+"""
