954433ac23a0b45078f8fe061372f1ee3f152f53
Qutaiba Olayyan <swat.ar123@gmail.com>
2024-05-24 16:34:22 +0300

    Test websockets

--- main.py
+++ main.py
@@ -723,6 +723,19 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.send_text(f"Message text was: {data}")
 
 
+# Test websockets
+# @app.websocket("/ws")
+# async def websocket(websocket: WebSocket):
+#     await websocket.accept()
+#     await websocket.send_json({"msg": "Hello WebSocket"})
+#     await websocket.close()
+
+# def test_websocket():
+#     client = TestClient(app)
+#     with client.websocket_connect("/ws") as websocket:
+#         data = websocket.receive_json()
+#         assert data == {"msg": "Hello WebSocket"}
+
 
 # Lifespan Events
 """
